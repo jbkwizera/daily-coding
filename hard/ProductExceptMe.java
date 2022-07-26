0cc0b69bca4b3f44db26ec980f8e13dc5667d6dc
jbkwizera <jeanbaptistekwi@gmail.com>
2022-07-26 10:03:01 +0200

    Beautify

--- hard/ProductExceptMe.java
+++ hard/ProductExceptMe.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class ProductExceptMe {
     public static int[] quadratic(int[] a) {
+        // Bruteforce quadratic solution w/out division.
         int N = a.length;
         int[] res = new int[N];
         java.util.Arrays.fill(res, 1);
@@ -13,6 +14,9 @@ public class ProductExceptMe {
         return res;
     }
     public static int[] linear1(int[] a) {
+        // "Bruteforce" linear solution. Find the product of all elements
+        // Then divide it by the i-th value. Trouble with this approach is
+        // it doesn't work if one of the values is 0.
         int N = a.length;
         int[] res = new int[N];
 
@@ -25,30 +29,28 @@ public class ProductExceptMe {
         return res;
     }
     public static int[] linear2(int[] a) {
+        // Linear solution without division
+        // Main idea is to create forward-way and backward-way products.
+        // Then forward[i-1] * backward[i+1] is the product that excludes
+        // the i-th index value.
+        
         int N = a.length;
         int[] res = new int[N];
-        int[] forward = new int[N];
-        int[] backward = new int[N];
-        forward[0] = a[0];
-        backward[N-1] = a[N-1];
-        for (int i = 1; i < N-1; i++) {
-            forward[i] = a[i] * forward[i-1];
-            backward[N-i-1] = a[N-i-1] * backward[N-i];
+        int[] forward  = new int[N+2];
+        int[] backward = new int[N+2];
+        forward[0]  = forward[N+1]  = 1;
+        backward[0] = backward[N+1] = 1;
+        for (int i = 1; i <= N; i++) {
+            forward[i]      = forward[i-1] * a[i-1];
+            backward[N-i+1] = backward[N-i+2] * a[N-i];
         }
-
-        res[0] = backward[1];
-        res[N-1] = forward[N-2];
-        for (int i = 1; i < N-1; i++)
-            res[i] = forward[i-1] * backward[i+1];
+        for (int i = 1; i <= N; i++)
+            res[i-1] = forward[i-1] * backward[i+1];
         return res;
-
-        // a          1,     2,    3,     4,      5
-        // foward     1,     2,    6,    24,    120
-        // backward 120,   120,   60,    20,      5
     }
 
     public static void main(String[] args) {
-        int N = 5;
+        int N = 10;
         int[] a = new int[N];
         Random random = new Random();
         for (int i = 0; i < N; i++) {
@@ -76,4 +78,4 @@ public class ProductExceptMe {
             System.out.printf("%6d ", lin2[i]);
         System.out.println();
     }
-}    
+}
